055398af8624dac20b0b32c0e034eaecea731d6b
Essar852 <essar@shopedge.tech>
2025-10-03 17:24:48 +0530

    - Build related changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",   // 👈 this enables static export
+  images: {
+    unoptimized: true, // 👈 avoids issues with Next Image Optimization
+  },
 };
 
 export default nextConfig;
